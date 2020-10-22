2a57b9a9827063fdfa47e629c5eb29b02c967b93
Joshua Whelan <josh25whelan@gmail.com>
2020-10-22 15:31:47 -0400

    render working

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-function start()  {
-        //create event listener for adding items
+const STORE = [
+    {name: "apples", checked: false},
+    {name: "oranges", checked: false},
+    {name: "milk", checked: true},
+    {name: "bread", checked: false}
+  ]
+
+  //function to render the shopping items list
+function renderShoppingList(){
+    let shoppingListItems = generateShoppingItems(STORE)
+
+    $('.js-shopping-list').html(shoppingListItems)
+    console.log(shoppingListItems)
+}
+//subfunction to create the list from the STORE for all items
+function generateShoppingItems(arr){
+    let results = ``
+    for(let i=0;i<arr.length;i++){
+        results = results + generateOneItemForList(arr[i])
+    }
+    return results
+}
+//subfunction to create one item for the list 
+function generateOneItemForList(obj){
+    let checkthrough = ``
+    if(obj.checked === true){
+        checkthrough = ` shopping-item__checked`
+    }
+    let template =`<li>
+    <span class="shopping-item${checkthrough}">${obj.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`
+  return template
+}
+
+
+
+/*        //create event listener for adding items
         $('#js-shopping-list-form').submit(function (evt) {
             evt.preventDefault()
             const addInput = $(this).find('input[name="shopping-list-entry"]').val()
@@ -28,6 +71,14 @@ function start()  {
           </button>
         </div>
       </li>`)
-    }
+    
+*/
+function startShoppingList(){
+    renderShoppingList()
+    //handleNewItemSubmit()
+   // handleItemCheckClicked()
+   // handleDeleteItemClicked()
+}
 
-$(start())
+
+$(startShoppingList)
